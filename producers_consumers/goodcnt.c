cc29d869b519a63391d7c82ecae71f4628d1958f
phattharaphon romphet <phattharaphon.r@ku.th>
2019-04-27 20:34:51 +0700

    test

--- producers_consumers/goodcnt.c
+++ producers_consumers/goodcnt.c
@@ -1,93 +1,7 @@
-// /*
-//  * goodcnt.c - A correctly synchronized counter program
-//  */
-// /* $begin goodcnt */
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <pthread.h>
-// #ifdef __APPLE__
-// #include <dispatch/dispatch.h>
-// #else
-// #include <semaphore.h>
-// #endif
-
-// #include "common_threads.h"
-
-// void *thread(void *vargp); /* Thread routine prototype */
-
-// /* Global shared variables */
-// /* $begin goodcntsemdef */
-// volatile long cnt = 0; /* Counter */
-
-// #ifdef __APPLE__
-// dispatch_semaphore_t mutex;          /* Semaphore that protects counter */
-// #else
-// sem_t mutex;
-// #endif
-// /* $end goodcntsemdef */
-
-// int main(int argc, char **argv)
-// {
-//     int niters;
-//     pthread_t tid1, tid2;
-
-//     /* Check input argument */
-//     if (argc != 2)
-//     {
-//         printf("usage: %s <niters>\n", argv[0]);
-//         exit(0);
-//     }
-//     niters = atoi(argv[1]);
-
-//     /* Create threads and wait for them to finish */
-//     /* $begin goodcntseminit */
-//     #ifdef __APPLE__
-//     mutex = dispatch_semaphore_create(1); // init with value of 1
-//     #else
-//     Sem_init(&mutex, 0, 1); /* mutex = 1 */
-//                             /* $end goodcntseminit */
-//     #endif
-//     Pthread_create(&tid1, NULL, thread, &niters);
-//     Pthread_create(&tid2, NULL, thread, &niters);
-//     Pthread_join(tid1, NULL);
-//     Pthread_join(tid2, NULL);
-
-//     /* Check result */
-//     if (cnt != (2 * niters))
-//         printf("BOOM! cnt=%ld\n", cnt);
-//     else
-//         printf("OK cnt=%ld\n", cnt);
-//     exit(0);
-// }
-
-// /* Thread routine */
-// void *thread(void *vargp)
-// {
-//     int i, niters = *((int *)vargp);
-//     static int numt = 0;
-
-//     printf("thread num = %d doing %d iterations\n", ++numt, niters);
-
-//     /* $begin goodcntthread */
-//     for (i = 0; i < niters; i++)
-//     {
-//         #ifdef __APPLE__
-//         dispatch_semaphore_wait(mutex, DISPATCH_TIME_FOREVER);
-//         #else
-//         P(&mutex);
-//         #endif
-//         cnt++;
-//         #ifdef __APPLE__
-//         dispatch_semaphore_signal(mutex);
-//         #else
-//         V(&mutex);
-//         #endif
-//     }
-//     /* $end goodcntthread */
-//     return NULL;
-// }
-// /* $end goodcnt */
-
+/*
+ * goodcnt.c - A correctly synchronized counter program
+ */
+/* $begin goodcnt */
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -98,7 +12,6 @@
 #endif
 
 #include "common_threads.h"
-#define N 2
 
 void *thread(void *vargp); /* Thread routine prototype */
 
@@ -116,7 +29,7 @@ sem_t mutex;
 int main(int argc, char **argv)
 {
     int niters;
-    pthread_t tid1, tid2, tid3, tid4;
+    pthread_t tid1, tid2;
 
     /* Check input argument */
     if (argc != 2)
@@ -136,16 +49,14 @@ int main(int argc, char **argv)
 #endif
     Pthread_create(&tid1, NULL, thread, &niters);
     Pthread_create(&tid2, NULL, thread, &niters);
-    Pthread_create(&tid3, NULL, thread, &niters);
-    Pthread_create(&tid4, NULL, thread, &niters);
     Pthread_join(tid1, NULL);
     Pthread_join(tid2, NULL);
-    Pthread_join(tid3, NULL);
-    Pthread_join(tid4, NULL);
 
     /* Check result */
-    printf("cnt = %ld\n", cnt);
-
+    if (cnt != (2 * niters))
+        printf("BOOM! cnt=%ld\n", cnt);
+    else
+        printf("OK cnt=%ld\n", cnt);
     exit(0);
 }
 
