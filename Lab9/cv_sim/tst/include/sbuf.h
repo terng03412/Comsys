e4f833b6fb9cf1dd382a3d51b80f0db3cfe5ee83
phattharaphon romphet <phattharaphon.r@ku.th>
2019-05-08 14:04:33 +0700

    m

--- Lab9/cv_sim/tst/include/sbuf.h
+++ Lab9/cv_sim/tst/include/sbuf.h
@@ -9,15 +9,15 @@
 /* $begin sbuft */
 typedef struct
 {
-  int *buf;    /* Buffer array */
-  int *id;     /* Thread number array */
-  int max;     /* Maximum number of slots */
-  int front;   /* buf[(front+1)%n] is first item */
-  int rear;    /* buf[rear%n] is last item */
-  sem_t mutex; /* Protects accesses to buf */
-  sem_t slots; /* Counts available slots */
-  sem_t items; /* Counts available items */
-  sem_t cond;
+  int *buf;              /* Buffer array */
+  int *id;               /* Thread number array */
+  int max;               /* Maximum number of slots */
+  int front;             /* buf[(front+1)%n] is first item */
+  int rear;              /* buf[rear%n] is last item */
+  pthread_mutex_t mutex; /* Protects accesses to buf */
+  pthread_mutex_t slots; /* Counts available slots */
+  pthread_mutex_t items; /* Counts available items */
+  pthread_mutex_t cond;
 } sbuf_t;
 
 void sbuf_init(sbuf_t *sp, int n);
